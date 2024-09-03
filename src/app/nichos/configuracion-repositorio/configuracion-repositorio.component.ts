1119a8866e027e405a0cf407e380c5308919fde7
Sergio Crúz Flores <scruz@fmf.mx>
2024-09-03 16:53:33 -0600

    Se sube ajustes martes

--- src/app/nichos/configuracion-repositorio/configuracion-repositorio.component.ts
+++ src/app/nichos/configuracion-repositorio/configuracion-repositorio.component.ts
@@ -5,6 +5,7 @@ import { ConfirmationService, MessageService } from 'primeng/api';
 import { FilesService } from '../services/files.service';
 import { forkJoin } from 'rxjs';
 import { BlogService } from '../services/blog.service';
+import { cleanText } from 'src/app/lib/helpers';
 
 @Component({
   selector: 'app-configuracion-repositorio',
@@ -50,6 +51,68 @@ export class ConfiguracionRepositorioComponent implements OnInit{
        this.loading = false;
     });
   }
+
+
+    /**
+     * Si hacemos una modificacion en el repositorio central tomamos el archivos y lo subimos al repo que tenemos ne nodejs
+     */
+    subirFileAlRepoPruebas(file: any){
+      let comando = `cp /Applications/XAMPP/htdocs/serflix${file.path}${file.file} server/nichos/repositorio${file.path}`;
+      let campo = {
+        _id: file._id,
+        $set: {
+          'repo': true,
+          'local': false,
+          'dev': false,
+          'prod': false
+        }
+      }
+
+      this.filesService.subirFile(comando, campo)
+          .subscribe(response=>{
+            file.repo = response.repo;
+            file.local = response.local;
+            file.dev = response.dev;
+            file.prod = response.prod;
+          })
+    }
+
+    /**
+     * Se sube file a pruebas
+     */
+    subirFileLocal(file: any){
+      let comando = `cp server/nichos/repositorio${file.path}${file.file} server/nichos/${cleanText(this.nicho.nombre)}${file.path}`;
+      let campo = {
+        _id: file._id,
+        $set: {
+              'local': true
+            }
+      }
+    
+      this.filesService.subirFile(comando, campo)
+          .subscribe(response=>{
+            file.local = response.local;
+          });
+  }
+    
+  /**
+    * Se sube el archivo a dev
+    */
+  subirFileDev(file: any){
+    let comando = `cp server/nichos/${cleanText(this.nicho.nombre)}${file.path}${file.file} /Applications/XAMPP/htdocs/${cleanText(this.nicho.nombre)}${file.path}`;
+    let campo = {
+      _id: file._id,
+      $set: {
+        'dev': true
+      }
+    }
+    
+    this.filesService.subirFile(comando, campo)
+        .subscribe(response=>{
+            file.dev = response.dev;
+        })
+  }
+        
 
   /**
     * Regresar a la configuracion de nicho
